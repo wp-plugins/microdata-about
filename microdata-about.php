5007c27e38b2b7a7b7f791667b170fc62d626add
antsanchez <antsanchez@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-26 11:06:28 +0000

    PHP code improved and simplified. Also the options are saved now in a single array
    git-svn-id: https://plugins.svn.wordpress.org/microdata-about/trunk@1033087 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- microdata-about.php
+++ microdata-about.php
@@ -3,16 +3,41 @@
  * Plugin Name: Microdata About
  * Plugin URI: http://antsanchez.com/blog/wp-plugin-microdata
  * Description: Serves Microdata
- * Version: 1.0.1
+ * Version: 1.0.2
  * Author: Antonio Sanchez
  * Author URI: http://antsanchez.com
  * Text Domain: microdata-about
  * Domain Path: microdata-about
- * License: A short license name. Example: GPL2
+ * License: GPL2 v2.0
  */
 
 /* Plugin Domain */
 load_plugin_textdomain('microdata-about', false, plugins_url('microdata-about') . '/languages' );
+
+/* Array for the foreach loop, just to spare some code */
+function microdata_array(){
+	return array("Company", "Logo", "Name", "Nickname", "Title", "Role", "Affiliation", "Street", "Locality", "Region", "Postal-Code", "Country", "Email", "Phone", "Fax", "URL");
+}
+
+/* Creates an array with the structure for the update_option() function */
+function microdata_array_structure(){
+	return array("Company" => null,
+			"Logo" => null,
+			"Name" => null,
+			"Nickname" => null,
+			"Title" => null,
+			"Role" => null,
+			"Affiliation" => null,
+			"Street" => null,
+			"Locality" => null,
+			"Region" => null,
+			"Postal-Code" => null,
+			"Country" => null,
+			"Email" => null,
+			"Phone" => null,
+			"Fax" => null,
+			"URL" => null);
+}
 
 /* About & Microformats */
 add_action('admin_menu', 'microdata_menu');
@@ -28,234 +53,153 @@ function microdata_menu() {
 
 /* About Form */
 function my_plugin_microdata(){
-    if ( !current_user_can( 'manage_options' ) )  {
-	wp_die( 'You do not have sufficient permissions to access this page.' );
-    }else if(isset($_POST["enviar"])){
-	echo __('Updated:', 'Blackcolors') . '<br>';
-	if($company = $_POST["namecompany"]){
-	    update_option("mccompany", $company);
-	    echo "<br>" . __('Name (Company): ', 'microdata-about') . $company;
-	}
-	if($logo = $_POST["upload_image"]){
-	    update_option("mclogo", $logo);
-	    echo "<br>" . __('Logo (URL): ', 'microdata-about') . $logo;
-	}
-	if($nombre = $_POST["name"]){
-	    update_option("mcname", $nombre);
-	    echo "<br>" . __('Name (Person): ', 'microdata-about') . $nombre;
-	}
-	if($nickname = $_POST["nickname"]){
-	    update_option("mcnickname", $nickname);
-	    echo "<br>" . __('Nickname: ', 'microdata-about') . $nickname;
-	}
-	if($title = $_POST["title"]){
-	    update_option("mctitle", $title);
-	    echo "<br>" . __('Title (Person): ', 'microdata-about') . $title;
-	}
-	if($role = $_POST["role"]){
-	    update_option("mcrole", $role);
-	    echo "<br>" . __('Role (Person): ', 'microdata-about') . $role;
-	}
-	if($affiliation = $_POST["affiliation"]){
-	    update_option("mcaffiliation", $affiliation);
-	    echo "<br>" . __('Affiliation (Person): ', 'microdata-about') . $affiliation;
-	}
-	if($street = $_POST["street"]){
-	    update_option("mcstreet", $street);
-	    echo "<br>" . __('Street: ', 'microdata-about') . $street;
-	}
-	if($locality = $_POST["locality"]){
-	    update_option("mclocality", $locality);
-	    echo "<br>" . __('Locality: ', 'microdata-about') . $locality;
-	}
-	if($region = $_POST["region"]){
-	    update_option("mcregion", $region);
-	    echo "<br>" . __('Region: ', 'microdata-about') . $region;
-	}
-	if($postalcode = $_POST["postal-code"]){
-	    update_option("mcpostalcode", $postalcode);
-	    echo "<br>" . __('Postal Code: ', 'microdata-about') . $postalcode;
-	}
-	if($country = $_POST["country-name"]){
-	    update_option("mccountry", $country);
-	    echo "<br>" . __('Country Name: ', 'microdata-about') . $country;
-	}
-	if($phone = $_POST["phone"]){
-	    update_option("mcphone", $phone);
-	    echo "<br>" . __('Phone: ', 'microdata-about') . $phone;
-	}
-	if($url = $_POST["url"]){
-	    update_option("mcurl", $url);
-	    echo "<br>" . __('WEB URL: ', 'microdata-about') . $url;
-	}
-	if($fax = $_POST["fax"]){
-	    update_option("mcfax", $url);
-	    echo "<br>" . __('FAX: ', 'microdata-about') . $fax;
-	}
-	if($email = $_POST["email"]){
-	    update_option("mcemail", $email);
-	    echo "<br>" . __('Email: ', 'microdata-about') . $email;
-	}
-    }else{
-		$name = get_option("mcname");
-		$logo = get_option("mclogo");
-		$nickname = get_option("mcnickname");
-		$title = get_option("mctitle");
-		$role = get_option("mcrole");
-		$affiliation = get_option("mcaffiliation");
-		$photo = get_option("mcphoto");
-		$company = get_option("mccompany");
-		$street = get_option("mcstreet");
-		$locality = get_option("mclocality");
-		$region = get_option("mcregion");
-		$postalcode = get_option("mcpostalcode");
-		$country = get_option("mccountry");
-		$phone = get_option("mcphone");
-		$url = get_option("mcurl");
-		$fax = get_option("fax");
-		$email = get_option("email");
 
-	echo '<h2>' . __('Microdata About', 'microdata-about') . '</h2>';
-	echo '<p>' .__('More info about the Microdata on: <a href="https://support.google.com/webmasters/answer/176035" target="_blank">About Microdata, Google</a>', 'microdata-about') . '</p>';
-	echo '<form method="post" id="microdata">
-	<div id="col1"><h3>' . __('For Companies:', 'microdata-about') . '</h3>
-	    <label for="namecompany">' . __('Name (Company): ', 'microdata-about') . '</label><br>
-	    <input type="text" name="namecompany" maxlenght="60" id="namecompany" autocomplete="namecompany" value="' . $company . '" /><br>
-		<h3>' . __('For Persons:', 'microdata-about') . '</h3>
-		<label for="name">' . __('Name (Person): ', 'microdata-about') . '</label><br>
-	    <input type="text" name="name" maxlenght="60" id="name" autocomplete="name" value="' . $name . '" /><br>
-		<label for="nickname">' . __('Nickame: ', 'microdata-about') . '</label><br>
-	    <input type="text" name="nickname" maxlenght="60" id="nickname" autocomplete="nickname" value="' . $nickname . '" /><br>
-		<label for="title">' . __('Title: ', 'microdata-about') . '</label><br>
-	    <input type="text" name="title" maxlenght="60" id="title" autocomplete="title" value="' . $title . '" /><br>
-		<label for="role">' . __('Role: ', 'microdata-about') . '</label><br>
-	    <input type="text" name="role" maxlenght="60" id="role" autocomplete="role" value="' . $role . '" /><br>
-		<label for="affiliation">' . __('Affiliation: ', 'microdata-about') . '</label><br>
-	    <input type="text" name="affiliation" maxlenght="60" id="affiliation" autocomplete="affiliation" value="' . $affiliation . '" /><br>
-		<h3>' . __('Logo or Profile Image:', 'microdata-about') . '</h3>
-		<label for="upload_image">' . __('Image URL: *Recomended: 300x300 px', 'microdata-about') . '</label><br>
-	    <input id="upload_image" type="text" size="36" name="upload_image" id="upload_image" /><br>
-		<input id="upload_image_button" type="button" value="Upload Image" />
-		</div><div id="col2"><h3>' . __('Address:', 'microdata-about') . '</h3>
-		<label for="street">' . __('Street: ', 'microdata-about') . '</label><br>
-	    <input type="text" name="street" maxlenght="60" id="street" autocomplete="street" value="' . $street . '" /><br>
-	    <label for="locality">' . __('Locality: ', 'microdata-about') . '</label><br>
-	    <input type="text" name="locality" maxlenght="60" id="locality" autocomplete="locality" value="' . $locality . '" /><br>
-	    <label for="region">' . __('Region: ', 'microdata-about') . '</label><br>
-	    <input type="text" name="region" maxlenght="60" id="region" autocomplete="region" value="' . $region . '"/><br>
-	    <label for="postal-code">' . __('Postal Code: ', 'microdata-about') . '</label><br>
-	    <input type="text" name="postal-code" maxlenght="12" id="postal-code" autocomplete="postal-code" value="' . $postalcode . '" /><br>
-	    <label for="country-name">' . __('Country Name: ', 'microdata-about') . '</label><br>
-	    <input type="text" name="country-name" maxlenght="12" id="country-name" autocomplete="country-name" value="' . $country . '" /><br>
-	    <label for="Phone">' . __('Phone: ', 'microdata-about') . '</label><br>
-	    <input type="text" name="phone" maxlenght="25" id="phone" autocomplete="phone" value="' . $phone . '" /><br>
-	    <label for="Fax">' . __('Fax: ', 'microdata-about') . '</label><br>
-	    <input type="text" name="fax" maxlenght="25" id="fax" autocomplete="fax" value="' . $fax . '" /><br>
-	    <label for="URL">' . __('WEB URL: ', 'microdata-about') . '</label><br>
-	    <input type="url" name="url" maxlenght="60" id="url" autocomplete="url" value="' . $url . '" /><br>
-	    <label for="Email">' . __('Email: ', 'microdata-about') . '</label><br>
-	    <input type="email" name="email" maxlenght="60" id="email" autocomplete="email" value="' . $email . '" /><br></div>
-	    <div class="limpiartodo"><input type="submit" id="enviar" name="enviar" /></div>';
+    if ( !current_user_can( 'manage_options' ) )  {
+
+		wp_die( 'You do not have sufficient permissions to access this page.' );
+
+    }else if(isset($_POST["enviar"])){
+		
+		$microdata_array_fields = microdata_array();
+		$microdata_array_saved = microdata_array_structure();
+		foreach($microdata_array_fields as $valor){
+			if(!empty($_POST[$valor])){
+				$microdata_array_saved[$valor] = $_POST[$valor];
+				echo "Updated $valor : " . $microdata_array_saved[$valor] . "<br>";
+			}
+		}
+		update_option("microdata_data_saved", $microdata_array_saved);
+
+    }else{
+		echo '<div class="wrap">';
+		echo '<h2>' . __('Microdata About', 'microdata-about') . '</h2>';
+		echo '<p>' .__('More info about the Microdata on: <a href="https://support.google.com/webmasters/answer/176035" target="_blank">About Microdata, Google</a>', 'microdata-about') . '</p>';
+		echo '<form method="post" id="microdata">';
+	
+		$microdata_array_fields = microdata_array();
+		$microdata_array_saved = get_option("microdata_data_saved");
+		
+		echo '<div id="col1">';
+		$i = 0;
+		foreach($microdata_array_fields as $valor){
+			
+			if($i == 7){
+				echo '</div><div id="col2">';
+			}
+			
+			if(isset($microdata_array_saved[$valor])){
+				$value = "value='$microdata_array_saved[$valor]'";
+			}else{
+				$value = "";
+			}
+
+			if($valor == "URL"){
+				$type = "type='url'";
+			}else if($valor == "Email"){
+				$type = "type='email'";
+			}else{
+				$type = "type='text'";
+			}
+
+			echo "<label for='$valor'>$valor</label>";
+			echo "<input $type name='$valor' id='$valor' $value /><br>";
+
+			if($valor == "Logo"){
+				echo '<input id="Logo_button" type="button" value="Upload Image" /><br>';
+			}
+
+			$i++;
+
+		}
+		echo '</div>';
+
+		echo '<div class="limpiartodo"><input type="submit" id="enviar" name="enviar" /></div>';
+	    echo '</div>';		
     }
 }
 
 /* Serves Microdata */
 function microdata_address($atts){
+	
 	wp_enqueue_style( 'estilo', plugins_url('microdata-about') .'/css/estilo.css');
 
-	$name = get_option("mcname");
-	$logo = get_option("mclogo");
-	$nickname = get_option("mcnickname");
-	$title = get_option("mctitle");
-	$role = get_option("mcrole");
-	$affiliation = get_option("mcaffiliation");
-	$photo = get_option("mcphoto");
-	$company = get_option("mccompany");
-	$street = get_option("mcstreet");
-	$locality = get_option("mclocality");
-	$region = get_option("mcregion");
-	$postalcode = get_option("mcpostalcode");
-	$country = get_option("mccountry");
-	$phone = get_option("mcphone");
-	$url = get_option("mcurl");
-	$fax = get_option("fax");
-	$email = get_option("email");
+	$microdata_array_saved = get_option("microdata_data_saved");
 
 	$opcion = $atts['a'];
 
 	if($opcion == "person"){
 		$argumento = '<div itemscope itemtype="http://data-vocabulary.org/Person" class="mcdata">';
 		$argumento = $argumento . '<div class="mcname"><div class="mcname-content">';
-		if($name){
-			$argumento = $argumento . '<span itemprop="name">' . $name . '</span>';
+		if($microdata_array_saved["Name"]){
+			$argumento = $argumento . '<span itemprop="name">' . $microdata_array_saved["Name"] . '</span>';
 		}
-		if($nickname){
-			$argumento = $argumento . '&nbsp;<span itemprop="nickname">(' . $nickname . ')</span>';
+		if($microdata_array_saved["Nickname"]){
+			$argumento = $argumento . '&nbsp;<span itemprop="nickname">(' . $microdata_array_saved["Nickname"] . ')</span>';
 		}
 		$argumento = $argumento . '</div></div>'; /* END .mcname */
-		if($logo){
-			$argumento = $argumento . '<div class="mcphoto"><img src="' . $logo . '" itemprop="photo"></div>';
+		if($microdata_array_saved["Logo"]){
+			$argumento = $argumento . '<div class="mcphoto"><img src="' . $microdata_array_saved["Logo"] . '" itemprop="photo"></div>';
 		}
 		$argumento = $argumento . '<div class="mcperson">';
-		if($title){
-			$argumento = $argumento . '<span itemprop="title">' . $title . '</span><br>';
+		if($microdata_array_saved["Title"]){
+			$argumento = $argumento . '<span itemprop="title">' . $microdata_array_saved["Title"] . '</span><br>';
 		}
-		if($role){
-			$argumento = $argumento . '<span itemprop="role">' . $role . '</span><br>';
+		if($microdata_array_saved["Role"]){
+			$argumento = $argumento . '<span itemprop="role">' . $microdata_array_saved["Role"] . '</span><br>';
 		}
-		if($affiliation){
-			$argumento = $argumento . '<span itemprop="affiliation">' . $affiliation . '</span>';
+		if($microdata_array_saved["Affiliation"]){
+			$argumento = $argumento . '<span itemprop="Affiliation">' . $affiliation . '</span>';
 		}
 		$argumento = $argumento . '</div>'; /* END .mcperson */
 	}else{
 		$argumento = '<div itemscope itemtype="http://schema.org/Organization" class="mcdata">';
 		$argumento = $argumento . '<div class="mcname"><div class="mcname-content">';
-		if($company){
-			$argumento = $argumento . '<span itemprop="name">' . $company . '</span>';
+		if($microdata_array_saved["Company"]){
+			$argumento = $argumento . '<span itemprop="name">' . $microdata_array_saved["Company"] . '</span>';
 		}
 		$argumento = $argumento . '</div></div>'; /* 1: END .mcnamecontent; 2: END .mcname */
-		if($logo){
-			$argumento = $argumento . '<div class="mcphoto"><img src="' . $logo . '" itemprop="logo"></div>';
+		if($microdata_array_saved["Logo"]){
+			$argumento = $argumento . '<div class="mcphoto"><img src="' . $microdata_array_saved["Logo"] . '" itemprop="logo"></div>';
 		}
 	}
 	
 	$argumento = $argumento . '<div class="mcaddress">';
-	if($street or $locality or $postalcode or $region){
+	if($microdata_array_saved["Street"] or $microdata_array_saved["Locality"] or $microdata_array_saved["Postal-Code"] or $microdata_array_saved["Region"]){
 	    $argumento = $argumento . '<div itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">';
-	    if($street){
-		$argumento = $argumento . '<span itemprop="streetAddress">' . $street . '</span><br>';
+	    if($microdata_array_saved["Street"]){
+		$argumento = $argumento . '<span itemprop="streetAddress">' . $microdata_array_saved["Street"] . '</span><br>';
 	    }
-	    if($locality){
-		$argumento = $argumento . '<span itemprop="addressLocality">' . $locality . '</span>&nbsp;';
+	    if($microdata_array_saved["Locality"]){
+		$argumento = $argumento . '<span itemprop="addressLocality">' . $microdata_array_saved["Locality"] . '</span>&nbsp;';
 	    }
-	    if($region){
-		$argumento = $argumento . '(<span itemprop="addressRegion">' . $region . '</span>)<br> ';
+	    if($microdata_array_saved["Region"]){
+		$argumento = $argumento . '(<span itemprop="addressRegion">' . $microdata_array_saved["Region"] . '</span>)<br> ';
 	    }
-	    if($postalcode){
-		$argumento = $argumento . ' <span itemprop="postalCode">' . $postalcode . '</span>&nbsp;';
+	    if($microdata_array_saved["Postal-Code"]){
+		$argumento = $argumento . ' <span itemprop="postalCode">' . $microdata_array_saved["Postal-Code"] . '</span>&nbsp;';
 	    }
-	    if($country){
-		$argumento = $argumento . '<span itemprop="addresCountry">' . $country . '</span><br>';
+	    if($microdata_array_saved["Country"]){
+		$argumento = $argumento . '<span itemprop="addresCountry">' . $microdata_array_saved["Country"] . '</span><br>';
 	    }
 	    $argumento = $argumento . '</div>';
 	}
 	$argumento = $argumento . "</div>"; /* End .mcaddress */
 	
 	$argumento = $argumento . '<div class="mccontact">';
-	if($phone){
-	    $argumento = $argumento . '<div>Tel: <span itemprop="telephone">' . $phone . '</span></div>';
+	if($microdata_array_saved["Phone"]){
+	    $argumento = $argumento . '<div>Tel: <span itemprop="telephone">' . $microdata_array_saved["Phone"] . '</span></div>';
 	}
 	
-	if($fax){
-	    $argumento = $argumento . '<div>Fax: <span itemprop="fax">' . $fax . '</span></div>';
+	if($microdata_array_saved["Fax"]){
+	    $argumento = $argumento . '<div>Fax: <span itemprop="fax">' . $microdata_array_saved["Fax"] . '</span></div>';
 	}
 
-	if($url){
-	    $argumento = $argumento . '<div><a href="' . $url . '" itemprop="url">' . $url . '</a></div>';
+	if($microdata_array_saved["URL"]){
+	    $argumento = $argumento . '<div><a href="' . $microdata_array_saved["URL"] . '" itemprop="url">' . $microdata_array_saved["URL"] . '</a></div>';
 	}
 
-	if($email){
-		$argumento = $argumento . '<div><a href="mailto:' . $email . '" itemprop="email">' . $email . '</a></div>';
+	if($microdata_array_saved["Email"]){
+		$argumento = $argumento . '<div><a href="mailto:' . $microdata_array_saved["Email"] . '" itemprop="email">' . $microdata_array_saved["Email"] . '</a></div>';
 	}
 	
 	$argumento = $argumento . "</div></div>"; /* 1: End Address; 2: End Person OR Organization */
